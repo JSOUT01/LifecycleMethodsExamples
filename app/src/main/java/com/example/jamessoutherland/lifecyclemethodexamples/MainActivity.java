bd51a8df48ce92054bb018d93a840b75bae8ec1e
James Southerland <james.f.southerland@gmail.com>
2018-06-14 11:00:31 -0400

    Android application

--- app/src/main/java/com/example/jamessoutherland/lifecyclemethodexamples/MainActivity.java
+++ app/src/main/java/com/example/jamessoutherland/lifecyclemethodexamples/MainActivity.java
@@ -1,20 +1,60 @@
 package com.example.jamessoutherland.lifecyclemethodexamples;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //References Buttons located in XML associated with this activity.
+    private Button newActivityButton;
+    private Button finishProgramButton;
+
+    private Intent newActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show();
+        //Creates Small popup at the bottom of the screen
+        Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show();
 
+        //Links the variable reference to a specific UI element by id (name)
+        newActivityButton = findViewById(R.id.start_new_activity);
+        finishProgramButton = findViewById(R.id.finish_activity_button);
+
+        //Setting Intent to navigate from THIS activity to a different one
+        newActivity = new Intent(this, SecondActivity.class);
+
+        //Methods to initiate onClickListeners for the buttons in the UI
+        setListeners();
     }
 
+    private void setListeners() {
+        //Sets listener for newActivityButton only
+        newActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Takes our intent and tells it to use that to start a new Activity
+                startActivity(newActivity);
+
+
+            }
+        });
+        //Sets listener for finishProgramButton only
+        finishProgramButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Closes current activity (in this location this called will close our program)
+                finish();
+            }
+        });
+}
+//Lifecycle methods listed below
     @Override
     protected void onDestroy() {
         super.onDestroy();
--- app/src/main/java/com/example/jamessoutherland/lifecyclemethodexamples/SecondActivity.java
+++ app/src/main/java/com/example/jamessoutherland/lifecyclemethodexamples/SecondActivity.java
@@ -2,12 +2,31 @@ package com.example.jamessoutherland.lifecyclemethodexamples;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private Button finishButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        //Links variable reference to UI element for this activity by Id
+        finishButton = findViewById(R.id.finish_button);
+
+        setListener();
+    }
+
+    private void setListener() {
+
+        finishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
